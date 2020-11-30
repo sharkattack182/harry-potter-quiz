36ce3ad20f5a225a73689b84c49f589bb4c5bdeb
Matthew Courtney <matthewdc182@gmail.com>
2020-11-30 17:07:24 -0500

    added a navbar and set the display none event listener for the start button

---
+++ script.js
@@ -0,0 +1,6 @@
+var startBtn = document.querySelector(".start");
+var welcomeEl = document.querySelector(".welcome")
+
+startBtn.addEventListener("click", function() {
+    welcomeEl.style.display = "none";
+})
