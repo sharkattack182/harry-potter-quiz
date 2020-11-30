fb6762df41b81c1bc5a27467a0fc812277653a97
Matthew Courtney <matthewdc182@gmail.com>
2020-11-30 17:36:59 -0500

    added in a startGame function that starts a timer and displays the first questition

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 var startBtn = document.querySelector(".start");
 var welcomeEl = document.querySelector(".welcome");
 var questionsEl = document.querySelector(".questions");
+var timerEl = document.querySelector(".timer");
+
+var secondsLeft = 60;
+
+function startGame() {
+    welcomeEl.style.display = "none";
+    questionsEl.style.display = "block";
+
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft + "seconds left."
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+    }, 1000);
+}
 
 
 startBtn.addEventListener("click", function() {
-    welcomeEl.style.display = "none";
-    questionsEl.style.display = "block";
+    startGame();
 })
