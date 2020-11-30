323c0358e319c1cbe96ef1ee8481a5c9f159293f
Matthew Courtney <matthewdc182@gmail.com>
2020-11-30 17:15:15 -0500

    added in the questions content and the buttons set display to block when start button is clicked

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var startBtn = document.querySelector(".start");
-var welcomeEl = document.querySelector(".welcome")
+var welcomeEl = document.querySelector(".welcome");
+var questionsEl = document.querySelector(".questions");
+
 
 startBtn.addEventListener("click", function() {
     welcomeEl.style.display = "none";
+    questionsEl.style.display = "block";
 })
