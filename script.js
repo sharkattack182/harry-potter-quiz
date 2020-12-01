cd065ef41896f3c5f4e65dcccae72eae601e320f
Matthew Courtney <matthewdc182@gmail.com>
2020-11-30 23:04:31 -0500

    added in a questions object along with some notes for the outline and gave the buttons values to match up with answers later on

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+// targeting elements on the page
 var startBtn = document.querySelector(".start");
 var welcomeEl = document.querySelector(".welcome");
 var questionsEl = document.querySelector(".questions");
 var timerEl = document.querySelector(".timer");
 
+// targeting options
+var optionA = questionsEl.querySelector(".a");
+var optionB = questionsEl.querySelector(".b");
+var optionC = questionsEl.querySelector(".c");
+var optionD = questionsEl.querySelector(".d");
+
+// sets timer to start at 60 seconds
 var secondsLeft = 60;
 
+// questions objects with options and answers
+var questions = {
+    question: "which of the following is not not G?",
+    a: "A",
+    b: "B",
+    c: "C",
+    d: "G",
+    answer: "d"
+}
+
+// set the question number to zero will be incremented as the player answers questions
+var questionNumber = 0;
+
+// general funtion of the game
 function startGame() {
+
+// changes the dislay from the welcome page to the questions page
     welcomeEl.style.display = "none";
     questionsEl.style.display = "block";
 
+// setting a timer to end the game when it hits zero
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timerEl.textContent = secondsLeft + "seconds left."
@@ -20,7 +45,7 @@ function startGame() {
     }, 1000);
 }
 
-
+// initiates the quiz when player presses start
 startBtn.addEventListener("click", function() {
     startGame();
 })
